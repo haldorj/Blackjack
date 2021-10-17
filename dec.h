56cc11b5fbf23e764b5051dc6ef38956d6b952d1
haldorj <89477584+haldorj@users.noreply.github.com>
2021-10-17 04:57:29 +0200

    Update dec.h

--- dec.h
+++ dec.h
@@ -5,7 +5,7 @@ void log_in();		//function for user to log in.
 void rules();		//displays game rules.
 void player_draw();	//function for player to draw cards.
 void funds();		//function to manage bets and funds.
-void dealer_draw(); //function for house to draw cards.
+void dealer_draw();   //function for house to draw cards.
 void reset();		//function to end the round (new game).
 void player_ace();	//function for when player gets an ace.
 void dealer_ace();	//function for when house gets an ace.
